0fcba4a4e41436a6e66b65c557c88055567d8e03
Radouane Bahi <radbahi@gmail.com>
2020-01-28 17:04:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+callBack();
+});
+
+function callBack() {
+    document.querySelector('#text').innerHTML = 'This is really cool!'
+}
